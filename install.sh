b54c98b50503312752c31a58cc8e21571f9536ed
Djamseed Khodabocus <djamseed86@gmail.com>
2024-02-02 01:44:18 +0400

    feat: create symlinks for skhd and yabai

--- install.sh
+++ install.sh
@@ -77,7 +77,9 @@ ln -sfn "$PWD"/.config/1Password "$HOME/.config/1Password"
 ln -sfn "$PWD"/.config/alacritty "$HOME/.config/alacritty"
 ln -sfn "$PWD"/.config/mpv "$HOME/.config/mpv"
 ln -sfn "$PWD"/.config/nvim "$HOME/.config/nvim"
+ln -sfn "$PWD"/.config/skhd "$HOME/.config/skhd"
 ln -sfn "$PWD"/.config/tmux "$HOME/.config/tmux"
+ln -sfn "$PWD"/.config/yabai "$HOME/.config/yabai"
 ln -sfn "$PWD"/.config/starship.toml "$HOME/.config/starship.toml"
 
 # Set macOS preferences (sane defaults)
