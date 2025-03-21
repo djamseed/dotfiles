779cb49d9a72d825940253407e0c0cf5ea45d013
Djamseed Khodabocus <djamseed86@gmail.com>
2025-03-22 03:22:18 +0400

    feat: add completion for the dotnet CLI

--- .config/zsh/rc.d/009_completion.zsh
+++ .config/zsh/rc.d/009_completion.zsh
@@ -37,3 +37,22 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
+
+# zsh parameter completion for the dotnet CLI
+
+_dotnet_zsh_complete()
+{
+  local completions=("$(dotnet complete "$words")")
+
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+    _arguments '*::arguments: _normal'
+    return
+  fi
+
+  # This is not a variable assignment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
+}
+
+compdef _dotnet_zsh_complete dotnet
