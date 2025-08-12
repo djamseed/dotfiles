b2e59804a6f5fca0f3768f553927e25e2050e166
Djamseed Khodabocus <djamseed86@gmail.com>
2025-08-12 22:48:20 +0400

    fix: color issues

--- .config/zsh/rc.d/001_exports.zsh
+++ .config/zsh/rc.d/001_exports.zsh
@@ -36,7 +36,7 @@ export CLICOLOR=1
 export LSCOLORS=GxFxCxDxCxegedabagaced
 
 # Enable color support for GNU `ls`
-(($+commands[gls])) && (($+commands[vivid])) && export LS_COLORS=$(vivid generate rose-pine)
+(($+commands[gls])) && (($+commands[vivid])) && export LS_COLORS=$(vivid generate rose-pine-dawn)
 
 # Reconfigure applications to use the XDG Base Directory structure
 export CARGO_HOME=$XDG_DATA_HOME/cargo
