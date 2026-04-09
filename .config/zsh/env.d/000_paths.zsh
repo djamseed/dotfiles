ff1ca0cdf4359f0fd163522fdf4c5d747a6fda12
Djamseed Khodabocus <djamseed86@gmail.com>
2026-04-09 23:47:52 +0400

    chore: remove redundant paths

--- .config/zsh/env.d/000_paths.zsh
+++ .config/zsh/env.d/000_paths.zsh
@@ -3,16 +3,11 @@ typeset -U path cdpath fpath manpath
 
 # Set the list of directories that zsh searches for commands
 path=(
-    /Applications/Docker.app/Contents/Resources/bin(N)
-    /opt/{homebrew,local}/{,s}bin(N)
     /usr/local/{,s}bin(N)
     $HOMEBREW_PREFIX/opt/curl/bin(N)
     $HOMEBREW_PREFIX/opt/make/libexec/gnubin(N)
     $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin(N)
-    $GOPATH(N)
-    $GOBIN(N)
-    $CARGO_HOME/bin(N)
-    $PYENV_ROOT/bin(N)
+    /Applications/Docker.app/Contents/Resources/bin(N)
     $path
 )
 
